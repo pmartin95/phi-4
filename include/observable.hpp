f1b0208478f89951042bb29ee96b74dd0478381a
pmartin95 <martip8@rpi.edu>
2024-01-29 08:37:03 -0500

    average data

--- include/observable.hpp
+++ include/observable.hpp
@@ -1,5 +1,6 @@
 #include "lattice.hpp"
-
+#include <string>
+#pragma once
 class observable
 {
 protected:
@@ -7,13 +8,15 @@ protected:
     std::vector<double> local_data;
 
 public:
-    observable(/* args */);
+    inline observable(/* args */);
     virtual ~observable() = default;
     // Define when to collect data
     virtual bool shouldCollect(int step) const = 0;
 
     // Collect data
     virtual void collectData(const lattice &lat) = 0;
+    // Return data
+    virtual double averageData() = 0;
 };
 
 observable::observable(/* args */)
@@ -22,6 +25,7 @@ observable::observable(/* args */)
 
 class avePhiSquare : public observable
 {
+
 public:
     bool shouldCollect(int step) const override
     {
@@ -36,7 +40,16 @@ public:
                         { phiSquare += x * x; });
         collected_data.push_back(phiSquare / numLatticeSites);
     }
+    double averageData() override
+    {
+        double buffer = 0.0;
+        for (auto &x : collected_data)
+        {
+            buffer += x;
+        }
+        return buffer / collected_data.size();
+    }
 
 private:
-    static const int thermalizationSteps = 1000;
+    static const int thermalizationSteps = 3000;
 };
