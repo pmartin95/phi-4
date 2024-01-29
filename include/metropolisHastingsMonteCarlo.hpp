91a92f69a0b2fedb779cd45a0cb67c2ef2a914d6
pmartin95 <martip8@rpi.edu>
2024-01-29 08:55:57 -0500

    add lattice simulation

--- include/metropolisHastingsMonteCarlo.hpp
+++ include/metropolisHastingsMonteCarlo.hpp
@@ -1,9 +1,11 @@
-#pragma once
+
 #include <cmath>
+#include <memory>
 #include "action.hpp"
 #include "lattice.hpp"
 #include "randomUtils.hpp"
-
+#include "observable.hpp"
+#pragma once
 template <typename RNG, typename Distribution>
 int updateSite(lattice &Lattice, lagrangian_function L_func, RNG &rng, Distribution &dist, const double max_step, const double m2, const double g, int site_index)
 {
@@ -27,11 +29,27 @@ class metropolisHastingsMonteCarlo
 {
 private:
     int nAccepts, nRejects;
+    std::unique_ptr<lattice> SimulationLattice;
+    std::vector<std::unique_ptr<observable>> observables;
+    std::vector<int> latticeDims;
+    bool lattice_created = false;
+    bool lattice_thermalized = false;
     lagrangian_function lagrangian = phi4LangrangianDensityPeriodicBC;
     action_function action = phi4ActionPeriodicBC;
     double g, m2;
+    int thermalizationSteps = 3000, simulationSteps = 100000;
+    double max_step = 0.2;
+    std::mt19937 rng;
+    std::uniform_real_distribution<double> stepDistribution;
 
 public:
     metropolisHastingsMonteCarlo(/* args */);
+    metropolisHastingsMonteCarlo(double g_in, double m2_in, double max_step_in, int therm_steps_in, int sim_steps_in, std::vector<int> &lattice_dims_in);
     ~metropolisHastingsMonteCarlo();
+    void runSimulation();
+    void printObservableAverages();
+    void addObservable(std::unique_ptr<observable> observable)
+    {
+        observables.push_back(std::move(observable));
+    }
 };
--- src/metropolisHastingsMonteCarlo.cpp
+++ src/metropolisHastingsMonteCarlo.cpp
@@ -3,25 +3,46 @@
 metropolisHastingsMonteCarlo::metropolisHastingsMonteCarlo(/* args */)
 {
 }
-metropolisHastingsMonteCarlo::metropolisHastingsMonteCarlo(double g_in, double m2_in, double max_step_in) : g(g_in), m2(m2_in), max_step(max_step_in), stepDistribution(-max_step_in, max_step), nAccepts(0), nRejects(0)
+metropolisHastingsMonteCarlo::metropolisHastingsMonteCarlo(double g_in, double m2_in, double max_step_in, int therm_steps_in, int sim_steps_in, std::vector<int> &lattice_dims_in) : g(g_in), m2(m2_in), max_step(max_step_in), stepDistribution(-max_step_in, max_step), nAccepts(0), nRejects(0), simulationSteps(sim_steps_in), thermalizationSteps(therm_steps_in), latticeDims(lattice_dims_in)
 {
+    std::random_device r;
+    rng.seed(r());
+    SimulationLattice = std::unique_ptr<lattice>(new lattice(latticeDims));
+    fillLatticeWithRandom(*SimulationLattice, rng, stepDistribution);
+    lattice_created = true;
 }
 metropolisHastingsMonteCarlo::~metropolisHastingsMonteCarlo()
 {
 }
-
+void metropolisHastingsMonteCarlo::printObservableAverages()
+{
+    for (auto &x : observables)
+    {
+        std::cout << x->averageData() << std::endl;
+    }
+}
 void metropolisHastingsMonteCarlo::runSimulation()
 {
     int totalSteps = thermalizationSteps + simulationSteps;
     int step = 0;
     if (!lattice_created)
-        SimulationLattice = std::unique_ptr<lattice>(new lattice());
+    {
+        SimulationLattice = std::unique_ptr<lattice>(new lattice(latticeDims));
+        fillLatticeWithRandom(*SimulationLattice, rng, stepDistribution);
+    }
     if (!lattice_thermalized)
     {
         for (; step < thermalizationSteps; step++)
         {
             for (int i = 0; i < SimulationLattice->getNumSites(); i++)
                 updateSite(*SimulationLattice, lagrangian, rng, stepDistribution, max_step, m2, g, i);
+            for (auto &obs : observables)
+            {
+                if (obs->shouldCollect(step))
+                {
+                    obs->collectData(*SimulationLattice);
+                }
+            }
         }
     }
     else
@@ -33,5 +54,12 @@ void metropolisHastingsMonteCarlo::runSimulation()
     {
         for (int i = 0; i < SimulationLattice->getNumSites(); i++)
             updateSite(*SimulationLattice, lagrangian, rng, stepDistribution, max_step, m2, g, i);
+        for (auto &obs : observables)
+        {
+            if (obs->shouldCollect(step))
+            {
+                obs->collectData(*SimulationLattice);
+            }
+        }
     }
 }
